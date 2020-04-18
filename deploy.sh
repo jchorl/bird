21befb7820d5e8a462ffc540de63bc15c36f7e7e
Josh Chorlton <jchorlton@gmail.com>
2020-04-17 23:12:31 -0700

    fix tagging

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,5 @@ echo "$2" | docker login -u "$1" --password-stdin
 
 docker images
 
-docker tag jchorl/bird:latest-arm jchorl/bird:testing || true
-docker tag jchorl/bird:latest-amd64 jchorl/bird:testing || true
-docker push jchorl/bird:testing
+docker tag birdbuild-armv71:latest jchorl/bird:latest
+docker push jchorl/bird:latest
